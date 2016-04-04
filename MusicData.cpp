05e084f61683c9a66985421b649166fa0f66e53d
taniho_0707 <taniho_0707@yahoo.co.jp>
2016-04-04 14:25:35 +0900

    Fix data structure in Musicdata

--- MusicData.cpp
+++ MusicData.cpp
@@ -10,14 +10,14 @@ md::MusicData::~MusicData(){
 	
 }
 
-bool md::MusicData::setNote(uint16_t num, uint32_t d){
-	if(data.size() < num) return false;
-	data.at(num).all = d;
+bool md::MusicData::setNote(uint16_t num, struct note d){
+	if(data.size() < num) data.resize(num);
+	data.at(num) = d;
 	return true;
 }
 
-uint16_t md::MusicData::setNotes(uint16_t start, uint32_t* d, uint16_t length){
-	data.resize(start+length);
+uint16_t md::MusicData::setNotes(uint16_t start, struct note* d, uint16_t length){
+	if(data.size() < start+length) data.resize(start+length);
 	for(auto i = 0; i < length; ++i)
 		setNote(start+i, d[i]);
 	return length;
@@ -25,17 +25,28 @@ uint16_t md::MusicData::setNotes(uint16_t start, uint32_t* d, uint16_t length){
 
 md::notetype md::MusicData::getType(uint16_t num){
 	if(data.size() < num) return md::notetype::ERROR;
-	auto ret = data.at(num).part.type;
+	auto ret = data.at(num).type;
 	return static_cast<md::notetype>(ret);
 }
 
 uint32_t md::MusicData::getTime(uint16_t num){
 	if(data.size() < num) return 4294967295;
-	return data.at(num).part.time;
+	return data.at(num).time;
 }
 
 uint16_t md::MusicData::getNext(uint16_t num){
 	if(data.size() < num) return 65535;
-	return data.at(num).part.next;
+	return data.at(num).next;
+}
+
+md::notehand md::MusicData::getHand(uint16_t num){
+	if(data.size() < num) return md::notehand::ERROR;
+	return static_cast<md::notehand>(data.at(num).hand);
+}
+
+md::noteline md::MusicData::getLine(uint16_t num){
+	if(data.size() < num) return md::noteline::NONE;
+	return static_cast<md::noteline>(data.at(num).line);
+
 }
 
--- MusicData.h
+++ MusicData.h
@@ -7,12 +7,30 @@ namespace md{
 
 	enum class notetype : unsigned char {
 		SINGLE,
-		LONGSTART,
-		LONGEND,
-		SLIDESTART,
-		SLIDECONT,
-		SLIDEEND,
-		ERROR = 255
+		LONG_START,
+		LONG_END,
+		SLIDERIGHT_START,
+		SLIDERIGHT_CONT,
+		SLIDERIGHT_END,
+		SLIDELEFT_START,
+		SLIDELEFT_CONT,
+		SLIDELEFT_END,
+		ERROR = 15
+	};
+
+	enum class noteline : uint8_t {
+		LEFT,
+		LEFTMIDDLE,
+		MIDDLE,
+		RIGHTMIDDLE,
+		RIGHT,
+		NONE = 7
+	};
+
+	enum class notehand : uint8_t {
+		LEFT,
+		RIGHT,
+		ERROR = 3
 	};
 
 	/* ノーツデータ
@@ -24,29 +42,36 @@ namespace md{
 	      in ms
 	   3. 次のノーツ番号
 	      10bit
+	   4. 演奏腕
+	      1bit
+		  右，左
+	   5. ノーツライン
+	      3bit
 	*/
-	union note {
-		struct {
-			unsigned type : 4;
-			unsigned time : 18;
-			unsigned next : 10;
-		} part;
-		uint32_t all;
+	struct note {
+		unsigned type : 4;
+		unsigned time : 18;
+		unsigned next : 10;
+		unsigned hand : 2;
+		unsigned line : 3;
+		unsigned : 28;
 	} __attribute__ ((__packed__));
 
 	class MusicData {
 	private:
-		std::vector<union note> data;
+		std::vector<struct note> data;
 		
 	public:
 		MusicData();
 
-		bool setNote(uint16_t, uint32_t);
-		uint16_t setNotes(uint16_t, uint32_t*, uint16_t);
+		bool setNote(uint16_t, struct note);
+		uint16_t setNotes(uint16_t, struct note*, uint16_t);
 
 		notetype getType(uint16_t);
 		uint32_t getTime(uint16_t);
 		uint16_t getNext(uint16_t);
+		notehand getHand(uint16_t);
+		noteline getLine(uint16_t);
 		
 		~MusicData();
 	};
